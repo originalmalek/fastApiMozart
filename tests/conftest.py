0e7c19a3be83ab80eb09ce2bfa1006ca1e5f8879
originalmalek <originalmalek@gmail.com>
2024-02-26 12:03:50 +0100

    Add async client

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,8 +8,10 @@ from sqlalchemy import insert
 
 from config import settings
 from database import Base, async_engine, async_session
+from main import app as fastapi_app
 from users.models import ExchangeKeys, User
 
+from httpx import AsyncClient
 
 @pytest_asyncio.fixture(autouse=True, scope='session')
 async def prepare_database():
@@ -37,3 +39,9 @@ def event_loop(request):
     loop = asyncio.get_event_loop_policy().new_event_loop()
     yield loop
     loop.close()
+
+
+@pytest.fixture(scope='function')
+async def ac():
+    async with AsyncClient(app=fastapi_app, base_url='http://test') as ac:
+        yield ac
